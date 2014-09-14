33a77b2eb78d0e6c54ea375d5c356a84a6a147ec
pmosconi <pmosconi@gmail.com>
2014-09-14 19:08:39 +0200

    added myMicroView.h

---
+++ myMicroView.h
@@ -0,0 +1,13 @@
+// handy definitions for uView 
+
+// Use Serial Console for debugging
+//#define SERIALDEBUG
+
+// Fonts
+#define FONT_font5x7			0
+#define FONT_font8x16			1
+#define FONT_sevensegment		2
+#define FONT_fontlargenumber	3
+#define FONT_space01			4
+#define FONT_space02			5
+#define FONT_space03			6
